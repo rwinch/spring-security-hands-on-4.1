5cc0ea074081a4a7e966d39b4ccb44a6c47674d2
Rob Winch <rwinch@gopivotal.com>
2015-09-15 09:01:27 -0400

    Custom loginPage

--- src/main/java/sample/config/SecurityConfig.java
+++ src/main/java/sample/config/SecurityConfig.java
@@ -1,9 +1,24 @@
 package sample.config;
 
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+
+		http
+			.authorizeRequests()
+				.antMatchers("/resources/**").permitAll()
+				.anyRequest().authenticated()
+				.and()
+			.formLogin()
+				.loginPage("/")
+				.permitAll()
+				.and()
+			.httpBasic();
+	}
 }
