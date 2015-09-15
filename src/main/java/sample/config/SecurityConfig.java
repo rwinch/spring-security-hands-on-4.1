5865535d35c21004b80634f45d7a2633010f022e
Rob Winch <rwinch@gopivotal.com>
2015-09-15 08:51:58 -0400

    Default log in page

---
+++ src/main/java/sample/config/SecurityConfig.java
@@ -0,0 +1,9 @@
+package sample.config;
+
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+}
