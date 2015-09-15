7701251a5358df97f95a70b93e29b8f3233c70c6
Rob Winch <rwinch@gopivotal.com>
2015-09-15 09:03:15 -0400

    Ignore polyglot/**

--- src/main/java/sample/config/SecurityConfig.java
+++ src/main/java/sample/config/SecurityConfig.java
@@ -1,11 +1,19 @@
 package sample.config;
 
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+	@Override
+	public void configure(WebSecurity web) throws Exception {
+		web
+			.ignoring()
+				.antMatchers("/polyglot/**");
+	}
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
